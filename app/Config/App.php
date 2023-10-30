4d76a5fb6e29e252e9a6c24b5f37b4ec2fb5b0a5
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2023-10-30 17:15:05 -0500

    correcciones redirecciones

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/codel4/index.php/';
+    public string $baseURL = 'http://localhost/codel4/index.php';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- app/Views/bienvenida.php
+++ app/Views/bienvenida.php
@@ -19,7 +19,7 @@
     </center>
 
     <!-- Formulario -->
-    
+
     <form class="form m-2 text-center" action="<?php echo base_url("/form") ?>" method="GET" enctype="multipart/form-data">
         <input type="text" name="nombre" placeholder="Nombre">
         <input type="text" name="apellido" placeholder="Apellido">
@@ -27,14 +27,18 @@
         <br>
         <br>
         <label for="">Cedula: </label>
-        <input type="text" name="cedula" id="cedula" maxlength="10">
+        <input type="text" name="cedula" id="cedula" maxlength="10" onkeypress="return validador(event)">
         <br>
         <input class="btn btn-success m-2" type="submit" value="Enviar" name="enviar">
 
     </form>
 
+    <footer>
+        <script src="<?php echo base_url(); ?>../assets/js/funciones.js"></script>
+    </footer>
 
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </html>
